43a182e52e471647c73a932ef90a8d130bd7f441
hwanginbeom <rydn2002@gmail.com>
2019-10-21 16:25:09 +0900

    191021 | source(C#) | FrmButton

---
+++ DevStandardControl/FrmButton.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FrmButton.aspx.cs" Inherits="DevStandardControl.FrmButton" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>버튼 컨트롤</title>
+</head>
+<body>
+  <form id="form1" runat="server">
+      <div>
+          <asp:TextBox ID"txtNum" runat="server" />
+          <asp:Button ID="btnUp" runat="server"
+              OnClick="btnUp_Click" Text=" 증가 " />
+          <asp:Button ID="btnDown" runat="server"
+              OnClick="btnDown_Click" Text=" 감소 " />
+    </form>
+</body>
+</html>
+
+
+5.8 | 실습_ Button 컨트롤을 사용해 명령어 실행하기
+5.8.1 소개
+
+버튼 컨트롤은 사용 빈도가 꽤 높다. 이 실습에서는 버튼을 클릭할 때마다 텍스트박스에 있는 숫자 값을 1 증가시키거나 1 감소시키는 코드를 작성해보자.
---
+++ DevStandardControl/FrmButton.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System;
+
+/*
+5.8 | 실습_ Button 컨트롤을 사용해 명령어 실행하기
+5.8.1 소개
+
+버튼 컨트롤은 사용 빈도가 꽤 높다. 이 실습에서는 버튼을 클릭할 때마다 텍스트박스에 있는 숫자 값을 1 증가시키거나 1 감소시키는 코드를 작성해보자.
+     */
+
+namespace DevStandardControl
+{
+    public partial class FrmButton : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                txtNum.Text = "0"; // 처음 로드할 때만 0으로 초기화
+            }
+        }
+        protected void btnUp_Click(object sender, EventArgs e)
+        {
+            txtNum.Text =
+                Convert.ToString(Convert.ToInt32(txtNum.Text) + 1);
+        }
+        protected void btnDown_Click(object sender, EventArgs e)
+        {
+            txtNum.Text =
+                Convert.ToString(int.Parse(txtNum.Text) - 1);
+        }
+    }
+}
