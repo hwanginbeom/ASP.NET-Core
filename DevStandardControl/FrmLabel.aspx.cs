1222abd8e0b8cbc1179c50c6a4d7f70b48edc902
hwanginbeom <rydn2002@gmail.com>
2019-10-21 13:38:29 +0900

    191021 | source(C#) | DevStandardControl (FrmLabel / FrmStandardControl)

---
+++ DevStandardControl/FrmLabel.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+// 웹 페이지에 레이블을 떨어뜨려 놓고 해당 레이블에 웹 브라우저가 로드되는 순간의 서버 측 시간을 구해서 출력해보자.
+
+namespace DevStandardControl
+{
+    public partial class FrmLabel : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.lblDateTime.Text = DateTime.Now.ToString();
+
+        }
+    }
+}
