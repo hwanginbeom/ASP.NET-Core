8d8c0e13a9bacdb259320ef9f990f32c77ef8fc6
hwanginbeom <rydn2002@gmail.com>
2019-10-29 08:59:10 +0900

    191029 | source(C#)+explain | FrmimageMap

---
+++ DevStandardControl/6.7_FrmImageMap.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+/*
+
+
+6.6 | ImageMap 컨트롤
+6.6.1 소개
+
+이미지맵 컨트롤은 HTML의 이미지맵 기능을 좀 더 사용하기 편하게 만들어 놓은 ASP.NET 서버 컨트롤이다. 
+ 
+
+속성
+
+ImageUrl - 이미지맵에 사용할 이미지의 경로
+
+NavigatorUrl - 정해진 영역을 클릭했을 때 이동할 URL 경로
+
+AlternateText - 이미지가 표시되지 않을 때 나타나는 대체 텍스트
+
+HotSpots - 사각형, 원, 다각형으로 이미지맵 설정 시 좌표 지정
+
+
+이벤트
+
+Click - 이미지맵 컨트롤을 클릭했을 때 발생
+
+
+ */
+
+namespace DevStandardControl
+{
+    public partial class _6__7_FrmImageMap : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
